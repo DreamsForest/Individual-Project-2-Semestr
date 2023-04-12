261ca1eaef1600ce546c328ec97165486b7f73d9
Vitaly Muhin <muhinv451@gmail.com>
2023-04-12 17:18:33 +0300

    1.2

--- my-project-2-semestr/public/parallax-scrolling-website/ready-html/js/app.js
+++ my-project-2-semestr/public/parallax-scrolling-website/ready-html/js/app.js
@@ -6,3 +6,16 @@ ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 })
+
+function updateProgressBar() {
+	const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+	const documentHeight = document.documentElement.scrollHeight;
+	const scrollTop = window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
+	
+	const percent = (scrollTop / (documentHeight - windowHeight)) * 100;
+	
+	const progressBar = document.querySelector('.progress-bar');
+	progressBar.style.width = `${percent}%`;
+  }
+  
+  window.addEventListener('scroll', updateProgressBar);
